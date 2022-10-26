86b156c7d77d97ce6b227758ee96bea4707671a2
Lalo Guzmán <eduardo.guzman.m@outlook.com>
2022-10-26 16:16:15 -0300

    Commit inicial

---
+++ src/main/java/com/mycompany/appcarrent/AppCarRent.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.appcarrent;
+
+/**
+ *
+ * @author Lalo Guzmán
+ */
+public class AppCarRent {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
